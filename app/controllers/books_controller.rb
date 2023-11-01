5de6724312b11f16d96ad877cc344fa551c1afe1
tsubasa <ec2-user@ip-172-31-24-133.ap-northeast-1.compute.internal>
2023-11-01 08:52:15 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,22 +20,38 @@ class BooksController < ApplicationController
    # 以下を追加
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
+    # book.save
     #4 redirect_to '/book' を削除して、以下コードに変更
     # 詳細画面へリダイレクト
-    redirect_to book_path(book.id)
+    # redirect_to book_path(book.id)
+     # 5. トップ画面へリダイレクトし、フラッシュメッセージを表示させる
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:notice] = "error"
+      render :new
+    end
+
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:notice] = "error"
+      render :edit
+    end
+
   end
 
   def destroy
- 　book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy
     redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
 
